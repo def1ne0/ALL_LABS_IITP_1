a22fd342224a87bc513032f5964efdee62316b48
Arseniy Finevich <clumserinri537@gmail.com>
2026-02-08 23:46:02 +0300

    sosi anton

---
+++ dont_open_this_shit/target/release/ALL_LABS.d
@@ -0,0 +1,1 @@
+/home/define/Документы/ALL_LABS/target/release/ALL_LABS: /home/define/Документы/ALL_LABS/src/main.rs
---
+++ dont_open_this_shit/target/release/deps/ALL_LABS-2ef62fabde7996f3.d
@@ -0,0 +1,5 @@
+/home/define/Документы/ALL_LABS/target/release/deps/ALL_LABS-2ef62fabde7996f3.d: src/main.rs
+
+/home/define/Документы/ALL_LABS/target/release/deps/ALL_LABS-2ef62fabde7996f3: src/main.rs
+
+src/main.rs:
